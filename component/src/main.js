55816bbf6529950e1869184c156bad98dbb48b9a
AlexanderShushunov <AShushunov@mail.ru>
2019-04-19 23:32:00 +0300

    Items are passed as property

--- component/src/main.js
+++ component/src/main.js
@@ -1,7 +1,10 @@
 import Options from "./Options.html";
 
 var options = new Options({
-  target: document.body
+  target: document.body,
+  data: {
+    options: ["Svelte", "React", "Vue", "Angular"]
+  }
 });
 
 export default options;
